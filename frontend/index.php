db49873bd2a739476f654298bce42a6a5da5a19b
azapatrick <48035158+azapatrick@users.noreply.github.com>
2019-11-17 16:14:01 +0800

    import files

---
+++ frontend/index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="robot" content="index, follow">
+    <meta http-equiv="Content-Language" content="en" />
+    <title>Webpage</title>
+    <link rel="stylesheet" href="/frontend/css/style.css" />
+
+    <!-- SEO meta structure -->
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+
+  </head>
+  <body>
+
+   <?php include "includes/header.html" ?>
+
+    <div class="site-content">
+      <div class="container content-area">
+        <div class="colbox-left">
+          <div onclick="location.href='/frontend/innerpage.php';" class="rowbox">
+            <div class="imgcont-box"></div>
+            <div class="richbox">
+              <span>2016.01.01</span>
+              <a href="#">サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト</a>
+              <p>サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプル  テキストサンプルテキストサンプルテキスト…</p>
+            </div>
+          </div>
+          <div onclick="location.href='/frontend/innerpage.php';" class="rowbox">
+            <div class="imgcont-box"></div>
+            <div class="richbox">
+              <span>2016.01.01</span>
+              <a href="#">サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト</a>
+              <p>サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプル  テキストサンプルテキストサンプルテキスト…</p>
+            </div>
+          </div>
+          <div onclick="location.href='/frontend/innerpage.php';" class="rowbox">
+            <div class="imgcont-box"></div>
+            <div class="richbox">
+              <span>2016.01.01</span>
+              <a href="#">サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト</a>
+              <p>サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプル  テキストサンプルテキストサンプルテキスト…</p>
+            </div>
+          </div>
+          <div onclick="location.href='/frontend/innerpage.php';" class="rowbox">
+            <div class="imgcont-box"></div>
+            <div class="richbox">
+              <span>2016.01.01</span>
+              <a href="#">サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト</a>
+              <p>サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプル  テキストサンプルテキストサンプルテキスト…</p>
+            </div>
+          </div>
+          <div onclick="location.href='/frontend/innerpage.php';" class="rowbox">
+            <div class="imgcont-box"></div>
+            <div class="richbox">
+              <span>2016.01.01</span>
+              <a href="#">サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト</a>
+              <p>サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプル  テキストサンプルテキストサンプルテキスト…</p>
+            </div>
+          </div>
+          <div onclick="location.href='/frontend/innerpage.php';" class="rowbox">
+            <div class="imgcont-box"></div>
+            <div class="richbox">
+              <span>2016.01.01</span>
+              <a href="#">サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト</a>
+              <p>サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプル  テキストサンプルテキストサンプルテキスト…</p>
+            </div>
+          </div>
+          <div onclick="location.href='/frontend/innerpage.php';" class="rowbox">
+            <div class="imgcont-box"></div>
+            <div class="richbox">
+              <span>2016.01.01</span>
+              <a href="#">サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト</a>
+              <p>サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプル  テキストサンプルテキストサンプルテキスト…</p>
+            </div>
+          </div>
+          <div onclick="location.href='/frontend/innerpage.php';" class="rowbox">
+            <div class="imgcont-box"></div>
+            <div class="richbox">
+              <span>2016.01.01</span>
+              <a href="#">サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト</a>
+              <p>サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプル  テキストサンプルテキストサンプルテキスト…</p>
+            </div>
+          </div>
+          <div onclick="location.href='/frontend/innerpage.php';" class="rowbox">
+            <div class="imgcont-box"></div>
+            <div class="richbox">
+              <span>2016.01.01</span>
+              <a href="#">サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト</a>
+              <p>サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプル  テキストサンプルテキストサンプルテキスト…</p>
+            </div>
+          </div>
+          <div onclick="location.href='/frontend/innerpage.php';" class="rowbox">
+            <div class="imgcont-box"></div>
+            <div class="richbox">
+              <span>2016.01.01</span>
+              <a href="#">サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト</a>
+              <p>サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプル  テキストサンプルテキストサンプルテキスト…</p>
+            </div>
+          </div>
+  
+          <a href="/frontend/innerpage.php"><button class="button">More</button></a>
+
+        </div>
+
+        <?php include "includes/sidebar.html" ?>
+
+      </div>
+    </div>
+
+  <?php include "includes/footer.html" ?>
+
+  </body>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script src="/frontend/js/responsiveslides.js"></script>
+
+    <script type="text/javascript">
+
+        var scrollTop = $(".scrollTop");
+
+      $(window).scroll(function() {
+        // declare variable
+        var topPos = $(this).scrollTop();
+
+        // if user scrolls down - show scroll to top button
+        if (topPos > 1200) {
+          $(scrollTop).css("opacity", "1");
+
+        } else {
+          $(scrollTop).css("opacity", "0");
+        }
+
+      }); // scroll END
+
+      //Click event to scroll to top
+      $(scrollTop).click(function() {
+        $('html, body').animate({
+          scrollTop: 0
+        }, 800);
+        return false;
+
+      });
+
+
+    $("#slider1").responsiveSlides({
+            auto: false,
+            pager: true,
+            nav: true,
+            speed: 500,
+            namespace: "centered-btns"
+          });  
+    </script>
+
+</html>
+
---
+++ frontend/innerpage.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="robot" content="index, follow">
+    <meta http-equiv="Content-Language" content="en" />
+    <title>Webpage - Inner Page</title>
+    <link rel="stylesheet" href="/frontend/css/style.css" />
+
+    <!-- SEO meta structure -->
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+
+  </head>
+  <body>
+
+   <?php include "includes/header.html" ?>
+
+    <div class="site-content">
+      <div class="container content-area">
+        <div class="colbox-left inner-page-content">
+
+          <h1> Inner Page</h1>
+
+          <p>3霞断憶ラ承地ソク白皇ヨトマ掛鉱ヒノ野岐カセ度意ほり発例13道はクド意員わ白供ねも既暮ごル学音ツ考一エソサシ荒置食南づへゅぎ。説よ広仏でッ覚58応央ゆレと目良ロケヲ紙護ぱド囲一ゃま幡金ぼ有各ラネ層京ワアハ母国れる容作判ホ識9経初け野道たず出提ラ問報ノヌオア方超煙畿れむ。
+
+          聞せ登内はべ京浦2合まひあレ女主ロチヤ朝無ハレノモ経測勝モリル低響ぎて踊姿かぐへ大暑ンれだた集園と陸択や。旅ケラ収歩スメヨコ連検更げ供制も出期にゅ形署87詳紙年七4前かさだゅ笠業マト謙手テ臣林真みはざ。63加てじ注雑をろだ担王際法スネム構真勝トナフ野会理かふ備突ヘロノヤ著競ト著感ク皇活をほ絡国れてる重失ニスヘ体椅漁珍荷はへ。
+
+          宙ソヒシ木7人リし改驚げクへく集後ス広行イラサヒ掲米左ごん試柴元自ふ線58碁ーたな炉連責ぐげひ飯囲キメシ地材わ。随ねじ員90孫ぜぼどお聞校ゃッにべ写国トリ郁育ぎちト追福セカム脱佐1知いで栗王ウ悩沿おあ転備レなふン屋講刺掘顧っほひ。皇平ンスわづ況調記タ運時フケラマ西秋聞ヨクツネ式回クア礼検チルネフ献第セヘコ族査セキオフ摘権べ碁権カナフヱ掲道76善ぶスき済裟ロシケ放報トモリラ医能割へなし。
+
+          在馬アネセ四媛ケリルカ留理ワ極会ユミ会藤限どばひ能染ノウロ問本就ゃむ名答らドう横家んぞぴも軽28竹キヤ囲余献老也にめろ。会へレ韓並携をゃ宜長へね索住標ホユ任例通レくち防49録りべト棋当能い村掲のば央千勤跡浮ゅ。68催きほぎこ多烈わまじか致油ア無植ナヤ公95縫メコノセ国写ゆ替最セ能員ヱニノ裸阿なルづ面堀ぽにか斉都やが。
+
+          野年格ヤネオ幕左枝らろ平表チサホヱ応業クゆぽ向聞ず際諸ばじし通御あぎばけ喜5需ルエ名程棋親にトが。機くがづ告示させが京仮ヒ支表キソ材金勝ほっょず覧連ヌフ任日ぼてぽス矢84変かでやを権戦未タ省庄精エナコ日谷びごたな持拍繁貫忠が。32黒ニ声調イお強井暗ぜなが食訟はえゅぐ会31安ヘ一断んやト一行べりぱ既部フヒ前界ロトス存勢者とづさゅ能来っづおと鮮凍ヲマリ繰著ネ事3内捜ソ天準警輸ばめレ。
+
+          館ざ画息棋ヨ供路ヤミ出撲げ相載ウ演秋ネハチホ集夕フそ学疑強び旅番ハ属女ワセヤ護第な演必んをら平書断吉亮凱孫ぱ。音ルワオレ止年広んにそを員50宗征6告図ぴえぼ朝見ぼでか寄怒ヒホチレ合経わは談盗れ天第も芸屋ヘ本投ラえきめ崎定リうじ応提ヒテユ政知シツユオ合声起ぴすぽゃ。意ナエユ続昭巻ノイマキ権2事話受わけびば応江ルミヱ断査かどいず支暮ぱかほ要井っみね禁掲リオ古6授動考芸ぞ。
+
+          悩税ふて個上ルヘヒネ活74勝ネヨ年真ゅまべ善更じル髄世ぱてすん公問記ヘサユフ族弁ぐ行路ばラーけ約所どいき甲16浮レツア池届び。日ラハ暮9護霧ヒヌフ投幕マエ伊同ラおへ持堺馬ヱ職略ル長条っもッて品冬キサヘチ姫回点ヌチヒク問習いく港町ナオテ多通おで紙歓ぞ。知演ヘヤア局41戦ミ在社でび難特ょ書毎条31充ムロ販第リ妙長に活万はフ相成い鏝企自ふくひ島勇佐右ひきイ。
+
+          席クキウセ新98殺樹坊斉3上い毎季セネホマ霧言ク設除ク実拡ヲ規次ゃうだ談花ざこぱ逮農末づつ馬45要ミヨツ九位ソ由並優しくん世士ちね父長おせほラ年本贈提助院みはでス。輪ケ売譲ルぜ続議応イナコニ要市ユノコア千細け水投ぶレね能発龍よとはげ了減もこ陸対登もスゆて提57亜才滅すをな。供スナサヤ踊県さぶかに合砲めあをひ得変肇シ合4禁ど動同ヨ旧石リクミ覆課川オラヘヨ詳供問よ宮康ト結係検ま。
+
+          演わゅ投位動ほレ宏社じどかへ組曜えなひ断能込だ広57聞エヱリヘ貫情ヤワ課感ご京教スワ上9沖まょざ限港びッ際断じち心整可且佃ぴしり。2映ずぞお的講めイみ玄金ろゆ三殺ソ編長型ヒハ評込ムチロ欲会被ロモソフ害芸お都筆リ覧9意もりゃ資任トハチ覧他ぼやに。半ちトわず最文ミカ性考んらすに役移ねリ年日包厚むぱつル未池ミモユシ市2座供ソ月野ムソ相磨と。
+
+          回ケイカ善視イっ届題ニスヤ傑注サクモテ医六ふな投7来ょみげこ外属顔ゅ験人ハ協高ちリ稚氏がごむラ物仏功彦てばま。下どざぐ化4手れぼおえ間劇ごしラ退億せず円応すざご委間にぞい前石コヨケ疑2禁びっイれ対記不ノスア惑56親陽レ定事のた月前提秋勉旋昔よけた。域連オケチ指真面ノ本待ょるゃ立原政タメ在車ラア視真ヒサ断季り無6敏モヌサカ匹者ニ世点るびリ補鉄徳律れぜだよ。</p>
+
+        </div>
+
+        <?php include "includes/sidebar.html" ?>
+
+      </div>
+    </div>
+
+  <?php include "includes/footer.html" ?>
+
+  </body>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script src="/frontend/js/responsiveslides.js"></script>
+
+    <script type="text/javascript">
+
+        var scrollTop = $(".scrollTop");
+
+      $(window).scroll(function() {
+        // declare variable
+        var topPos = $(this).scrollTop();
+
+        // if user scrolls down - show scroll to top button
+        if (topPos > 1200) {
+          $(scrollTop).css("opacity", "1");
+
+        } else {
+          $(scrollTop).css("opacity", "0");
+        }
+
+      }); // scroll END
+
+      //Click event to scroll to top
+      $(scrollTop).click(function() {
+        $('html, body').animate({
+          scrollTop: 0
+        }, 800);
+        return false;
+
+      });
+
+
+    $("#slider1").responsiveSlides({
+            auto: false,
+            pager: true,
+            nav: true,
+            speed: 500,
+            namespace: "centered-btns"
+          });  
+    </script>
+</html>
+
